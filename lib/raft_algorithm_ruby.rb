e2025b33b74f399cf75329098a7e4595fb885bed
Alejo-Rey <alejo.rey.128@gmail.com>
2024-12-10 19:07:45 -0500

    initial comment

---
+++ lib/raft_algorithm_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "raft_algorithm_ruby/version"
+
+module RaftAlgorithmRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/raft_algorithm_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RaftAlgorithmRuby
+  VERSION = "0.1.0"
+end
